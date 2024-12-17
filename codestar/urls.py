e236053bad515cc294e6e65f216afd4daee00694
Arthur Vilela <arthur.vilela@gmail.com>
2024-12-17 16:18:32 +0000

    Fix url path in the wrong app

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
